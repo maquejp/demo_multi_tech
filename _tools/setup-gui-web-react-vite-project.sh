1bc7d92a8e77e294eb784860e43affd6b8826145
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2025-01-14 13:14:32 +0100

    chore: guis: web: react: vite: enhance setup script

--- _tools/setup-gui-web-react-vite-project.sh
+++ _tools/setup-gui-web-react-vite-project.sh
@@ -8,19 +8,21 @@ if [ $# -eq 0 ]; then
 fi
 
 PROJECT_NAME=$1
+BASE_PATH="../guis/web/reactjs/vite"
 
-if [ -d "../guis/web/reactjs/vite" ]; then
-  cd "../guis/web/reactjs/vite"
+if [ -d "$BASE_PATH" ]; then
+  echo "Setting up project..."
+  cd "$BASE_PATH"
 else
-  echo "Directory does not exist, creating it..."
-  mkdir -p "../guis/web/reactjs/vite"
-  cd "../guis/web/reactjs/vite"
+  echo "Project directory does not exist, creating it..."
+  mkdir -p "$BASE_PATH"
+  cd "$BASE_PATH"
 fi
 
 
 # Execute the commands with the provided project name
 echo "Creating new Vite project: $PROJECT_NAME"
-bun create vite@latest $PROJECT_NAME --template react-ts
+bun create vite@latest "$PROJECT_NAME" --template react-ts
 
 # Check if the previous command was successful
 if [ $? -ne 0 ]; then
@@ -28,26 +30,30 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Setting up project..."
-cd "$PROJECT_NAME"
+if [ -d "$PROJECT_NAME" ]; then
+  cd "$PROJECT_NAME"
+  echo "Setting up project..."
 
-echo "Installing dependencies..."
-bun install
+  echo "Installing dependencies..."
+  bun install
 
-echo "Installing Tailwind CSS..."
-bun install -d tailwindcss postcss autoprefixer
+  echo "Installing Tailwind CSS..."
+  bun install -d tailwindcss postcss autoprefixer
 
-echo "Initializing Tailwind CSS..."
-bunx tailwindcss init -p
+  echo "Initializing Tailwind CSS..."
+  bunx tailwindcss init -p
 
-echo "Configuring Tailwind CSS..."
-sed -i '1i @tailwind base;\n@tailwind components;\n@tailwind utilities;\n' ./src/index.css
+  echo "Configuring Tailwind CSS..."
+  sed -i '1i @tailwind base;\n@tailwind components;\n@tailwind utilities;\n' ./src/index.css
 
-echo "Updating Tailwind config..."
-sed -i '/content: \[\]/c\  content: [\n    "./index.html",\n    "./src/**/*.{js,ts,jsx,tsx}",\n  ],' tailwind.config.js
+  echo "Updating Tailwind config..."
+  sed -i '/content: \[\]/c\  content: [\n    "./index.html",\n    "./src/**/*.{js,ts,jsx,tsx}",\n  ],' tailwind.config.js
 
-echo "Updating App.tsx..."
-sed -i '/<div>/a \ \ \ \ <h1 className="text-3xl font-bold underline">Doctor Appointment</h1>' src/App.tsx
+  echo "Updating App.tsx..."
+  sed -i "/<div>/a \ \ \ \ <h1 className=\"text-3xl font-bold underline\">$PROJECT_NAME</h1>" src/App.tsx
 
-echo "Starting development server..."
-bun run dev
+  echo "Starting development server..."
+  bun run dev
+else
+  echo "Directory does not exist..."
+fi
--- guis/web/reactjs/vite/doctor_management/src/App.tsx
+++ guis/web/reactjs/vite/doctor_management/src/App.tsx
@@ -9,8 +9,7 @@ function App() {
   return (
     <>
       <div>
-    <h1 className="text-3xl font-bold underline">Doctor Appointment</h1>
-    <h1 className="text-3xl font-bold underline">Doctor Appointment</h1>
+    <h1 className="text-3xl font-bold underline">doctor_management</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
